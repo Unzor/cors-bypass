e03a3d3d5bf9e222192371fb6347cc6f48549f39
Unzor <71789103+Unzor@users.noreply.github.com>
2021-11-09 15:26:28 -0600

    Create index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+var express=require('express');
+var app=express();
+var bodyParser=require('body-parser');
+app.use(express.json());
+app.use(require('express-all-allow')());
+var axios=require('axios');
+
+function method(a){
+  eval(`app.${a}('/${a}', async function(req, res){
+  var response = await axios.${a}(req.query.url, req.body);
+  res.send(response.data);
+  })`);
+};
+
+method('get');
+
+method('post');
+
+method('put');
+
+app.listen(8000);
